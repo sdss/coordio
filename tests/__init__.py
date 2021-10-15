048ccd839d1c924ddf7b2ed0cdfc8b6208fefde7
José Sánchez-Gallego <gallegoj@uw.edu>
2021-10-15 00:03:53 -0700

    Try adding mock to top level

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+import os
+import unittest.mock
+import urllib.request
+
+
+iers_data_file = os.path.join(os.path.dirname(__file__), 'data', 'finals2000A.data.csv')
+
+# Mock urllib.request.urlopen so that tests work offline and faster.
+unittest.mock.patch.object(urllib.request, 'urlopen',
+                           return_value=open(iers_data_file, 'rb'))
