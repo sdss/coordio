6a21ab04ab47143fc53075490785ad8e791fb323
José Sánchez-Gallego <gallegoj@uw.edu>
2021-10-14 23:24:32 -0700

    Fix faking urilopen

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@
 # @Filename: conftest.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
-from coordio.iers import IERS
 import pathlib
 import shutil
 import urllib.request
@@ -41,8 +40,6 @@ def mock_iers(mocker, iers_data_path, tmpdir):
 
     iers_data_file = iers_data_path / 'finals2000A.data.csv'
 
-    def copy_file(*_):
-        shutil.copyfile(iers_data_file, tmpdir / 'finals2000A.data.csv')
-
-    # Mock urllib.request.urlretrieve so that tests work offline and faster.
-    mocker.patch.object(urllib.request, 'urlretrieve', side_effect=copy_file)
+    # Mock urllib.request.urlopen so that tests work offline and faster.
+    mocker.patch.object(urllib.request, 'urlopen',
+                        return_value=open(iers_data_file, 'rb'))
