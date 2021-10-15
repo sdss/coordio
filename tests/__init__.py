b72caa21173446f98efe28c96c681997c36b6c75
José Sánchez-Gallego <gallegoj@uw.edu>
2021-10-15 00:10:44 -0700

    Getting desperate

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,7 @@ import os
 import unittest.mock
 import urllib.request
 
-
+print('aaaaa')
 iers_data_file = os.path.join(os.path.dirname(__file__), 'data', 'finals2000A.data.csv')
 
 # Mock urllib.request.urlopen so that tests work offline and faster.
