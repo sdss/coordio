0a1734c8f77a832fb5b2af7d93ddd8e3f9a5542b
José Sánchez-Gallego <gallegoj@uw.edu>
2021-10-27 15:17:14 -0700

    Remove test printout

--- src/coordio/iers.py
+++ src/coordio/iers.py
@@ -95,7 +95,6 @@ class IERS:
         if channel == 'finals':
             URL = BASE_URL + 'standard/csv/finals.data.csv'
             warnings.warn(f'Downloading IERS table from {URL}.', CoordIOUserWarning)
-            print(urllib.request.urlopen)
             url = urllib.request.urlopen(URL)
             data = url.read()
 
