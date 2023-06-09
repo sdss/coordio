de50cd8447c3b9716f9a1821272aa1c1a1a7097b
imedan <ilija.medan@gmail.com>
2023-06-09 10:54:37 -0400

    add APO and LCO to tests

--- tests/test_offset.py
+++ tests/test_offset.py
@@ -18,8 +18,22 @@ fmagloss = Moffat2dInterp()
 def test_all_flags():
     def  test_flags(flag, mag, mag_limits, lunation, waveName,
                     sky, offset_min_skybrightness, can_off):
+        # test APO
         delta_ra, delta_dec, offset_flag = object_offset(mag, mag_limits, lunation,
-                                                         waveName, fmagloss=fmagloss,
+                                                         waveName, 'APO', fmagloss=fmagloss,
+                                                         skybrightness=sky,
+                                                         offset_min_skybrightness=offset_min_skybrightness,
+                                                         can_offset=can_off)
+        assert offset_flag == flag
+        assert delta_dec == 0.
+        if flag > 0:
+            assert delta_ra == 0.
+        else:
+            assert delta_ra > 0.
+
+        # test LCO
+        delta_ra, delta_dec, offset_flag = object_offset(mag, mag_limits, lunation,
+                                                         waveName, 'LCO', fmagloss=fmagloss,
                                                          skybrightness=sky,
                                                          offset_min_skybrightness=offset_min_skybrightness,
                                                          can_offset=can_off)
@@ -34,8 +48,22 @@ def test_all_flags():
             can_off_arr = None
         else:
             can_off_arr = numpy.array([can_off, can_off])
+        # test APO
         delta_ra, delta_dec, offset_flag = object_offset(numpy.vstack((mag, mag)), mag_limits, lunation,
-                                                         waveName, fmagloss=fmagloss, safety_factor=0.1,
+                                                         waveName, 'APO', fmagloss=fmagloss, safety_factor=0.1,
+                                                         beta=5, FWHM=1.7, skybrightness=sky,
+                                                         offset_min_skybrightness=offset_min_skybrightness,
+                                                         can_offset=can_off_arr)
+        assert numpy.all(offset_flag == flag)
+        assert numpy.all(delta_dec == 0.)
+        if flag > 0:
+            assert numpy.all(delta_ra == 0.)
+        else:
+            assert numpy.all(delta_ra > 0.)
+
+        # test LCO
+        delta_ra, delta_dec, offset_flag = object_offset(numpy.vstack((mag, mag)), mag_limits, lunation,
+                                                         waveName, 'LCO', fmagloss=fmagloss, safety_factor=0.1,
                                                          beta=5, FWHM=1.7, skybrightness=sky,
                                                          offset_min_skybrightness=offset_min_skybrightness,
                                                          can_offset=can_off_arr)
